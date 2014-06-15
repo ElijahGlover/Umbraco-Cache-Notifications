5e7eb0ac3a06d780a9b864e560c731c83ed5a61a
Elijah Glover <elijah@elijah.com.au>
2014-06-15 20:28:05 +1000

    Added Db Init Script

---
+++ Umbraco.Core.Sync.QueryNotification/DatabaseInit.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[RpcNotification](
+	[CorrelationId] [uniqueidentifier] NOT NULL,
+	[FactoryId] [uniqueidentifier] NOT NULL,
+	[MachineName] [nvarchar](100) NULL,
+	[Timestamp] [datetime] NOT NULL,
+	[NotificationType] [int] NOT NULL,
+	[Payload] [nvarchar](4000) NULL,
+ CONSTRAINT [PK_RpcNotification]
+ PRIMARY KEY CLUSTERED ([CorrelationId] ASC)
+ WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+ ON [PRIMARY]
+) ON [PRIMARY]
